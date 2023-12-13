c1641c3b1c86a1861205b47af706aeb4c20759a0
Spondon33 <nurulaminkh33@gmail.com>
2023-12-13 21:26:58 +0600

    '!=' , '=='

--- Conditionals/cndls.py
+++ Conditionals/cndls.py
@@ -34,3 +34,29 @@ elif x > y:
     print("x is greater than y")
 else:
     print("x is equal to y")
+
+
+#4 or
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+
+#5
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+# we can also write,
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
