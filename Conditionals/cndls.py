978e9de5521c311d574d6e4a252c37f3229d2593
Spondon33 <nurulaminkh33@gmail.com>
2023-12-13 21:03:10 +0600

    if, elif, else

--- Conditionals/cndls.py
+++ Conditionals/cndls.py
@@ -10,3 +10,27 @@ if x > y:
     print("x is greater than y")
 if x == y:
     print("x is equal to y")
+
+
+#2 elif faster than using multiple ifs
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
+
+#3 else
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
