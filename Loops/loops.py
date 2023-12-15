030083f7aa5fb4a839fc59f88d15cee58da7a6ae
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 20:07:04 +0600

    list

--- Loops/loops.py
+++ Loops/loops.py
@@ -111,3 +111,25 @@ def meow(n):
 main()
 
 
+# Hogwarts
+
+#1 List
+students = ["Hermione", "Harry", "Ron"]
+
+print(students[0])
+print(students[1])
+print(students[2])
+
+
+#2
+students = ["Hermione", "Harry", "Ron"]
+
+for student in students:
+    print(student)
+
+
+#3
+students = ["Hermione", "Harry", "Ron"]
+
+for i in range(len(students)):
+    print(i + 1, students[i])
