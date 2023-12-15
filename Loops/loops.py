d449da757b5a1aa8008410b1b54dec197003f192
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 19:33:29 +0600

    while loop

--- Loops/loops.py
+++ Loops/loops.py
@@ -5,4 +5,28 @@ print("meow")
 
 
 #2 while loop
+i = 3
+while i != 0:
+    print("meow")
+    i = i - 1
 
+
+#3
+i = 1
+while i <= 3:
+    print("meow")
+    i = i + 1
+
+
+#4 start from 0 instead of 1
+i = 0
+while i < 3:
+    print("meow")
+    i = i + 1
+
+
+#5 i + 1 is written as += 1
+i = 0
+while i < 3:
+    print("meow")
+    i += 1
