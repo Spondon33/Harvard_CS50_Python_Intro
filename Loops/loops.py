00026c60b8ac62cf1d18aea4864c1b9cad55e12e
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 21:24:36 +0600

    nested loops

--- Loops/loops.py
+++ Loops/loops.py
@@ -185,3 +185,102 @@ students = [
 
 for student in students:
     print(student["name"], student["house"], student["patronous"], sep=", ")
+
+
+# Nested loops
+
+#1 Mario
+print("#")
+print("#")
+print("#")
+
+
+#2
+for _ in range(3):
+    print("#")
+
+
+#3
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+
+main()
+
+
+#4
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    print("#\n" * height, end="")
+
+
+main()
+
+
+#5
+def main():
+    print_row(4)
+
+
+def print_row(width):
+    print("?" * width)
+
+
+main()
+
+
+#6
+def main():
+    print_square(3)
+
+
+def print_square(size):
+
+    # For each row in square
+    for i in range(size):
+        # For each brick in row
+        for j in range(size):
+            # print brick
+            print("#", end="")
+        print()
+
+
+main()
+
+
+#7
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        print("#" * size)
+
+
+main()
+
+
+#8
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        print_row(size)
+
+
+def print_row(width):
+    print("#" * width)
+
+
+main()
