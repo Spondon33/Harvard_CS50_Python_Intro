27d131596db1e34a38103b776df1280fa6a66124
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 19:38:28 +0600

    defining new functions with while and for loops

--- Loops/loops.py
+++ Loops/loops.py
@@ -74,3 +74,40 @@ while True:
 
 for _ in range(n):
     print("meow")
+
+
+#8 defines a new meow function
+def main():
+    meow(3)
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
+
+
+#9
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
+
+
