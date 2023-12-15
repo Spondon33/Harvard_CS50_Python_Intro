adf3094babac1589ad25743b86035a11e9b17ac3
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 21:01:42 +0600

    dict, list of dicts

--- Loops/loops.py
+++ Loops/loops.py
@@ -133,3 +133,55 @@ students = ["Hermione", "Harry", "Ron"]
 
 for i in range(len(students)):
     print(i + 1, students[i])
+
+
+# Dictionaries or dict
+
+#1
+students= {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+
+
+#2 for loop will only show the keys
+students= {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student)
+
+
+#3 will print the houses too
+students= {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
+
+
+#4 list of dictionaries
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronous": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronous": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronous": "Jack Russel terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronous": None}
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronous"], sep=", ")
