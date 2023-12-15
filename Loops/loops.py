741e0ee04a6e91385644977874a40a814aa8cbe2
Spondon33 <nurulaminkh33@gmail.com>
2023-12-15 19:36:40 +0600

    for loop

--- Loops/loops.py
+++ Loops/loops.py
@@ -30,3 +30,47 @@ i = 0
 while i < 3:
     print("meow")
     i += 1
+
+
+# for loop
+
+#1
+for i in [0, 1, 2]:
+    print("meow")
+
+
+#2 sets a range of values
+# we can write _ insted of i as i is not used as a variable
+for _ in range(3):
+    print("meow")
+
+
+#3 will show meowmeowmeow
+print("meow" * 3)
+
+
+#4 \n prints each meow in seperate lines
+print("meow\n" * 3)
+
+
+#5 removes the default end at the end
+print("meow\n" * 3, end = "")
+
+
+#6
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
+
+
+#7
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
