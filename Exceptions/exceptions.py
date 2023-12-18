bb68ae496c1ce92229f47ddc121caaf3bb2fb65b
Spondon33 <nurulaminkh33@gmail.com>
2023-12-18 15:26:17 +0600

    ValueError

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -1,3 +1,4 @@
+
 # Syntax Error:
 
 # syntax error: problem in the written code.
@@ -9,4 +10,16 @@ print("Hello, world)
 
 # Correct:
 print("Hello, world")
+
+
+# Value Error:
+
+# will show value error if input is string
+# int() only accepts numbers, not string.
+
+x = int(input("What's x?"))
+print(f"x is {x}")
+
+
+
 
