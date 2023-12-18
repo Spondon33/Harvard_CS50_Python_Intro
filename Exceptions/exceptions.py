a4db2787f9122c9b4bc15455db0ce935d6eb1949
Spondon33 <nurulaminkh33@gmail.com>
2023-12-18 15:00:34 +0600

    syntax error

---
+++ Exceptions/exceptions.py
@@ -0,0 +1,12 @@
+# Syntax Error:
+
+# syntax error: problem in the written code.
+# unterminated: did not stop or finished the code.
+# literal: that is typed in.
+# syntax error has to be solved by self.
+
+print("Hello, world)
+
+# Correct:
+print("Hello, world")
+
