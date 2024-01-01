f5178c93cea019574b21dc41983595eefd6c3bfd
Spondon33 <nurulaminkh33@gmail.com>
2024-01-01 20:21:08 +0600

    from

--- Libraries/libraries.py
+++ Libraries/libraries.py
@@ -5,3 +5,12 @@ import random
 
 coin = random.choice(["heads", "tails"])
 print(coin)
+
+
+# from:
+
+# imports the choice function from the random module
+from random import choice
+
+coin = choice(["heads", "tails"])
+print(coin)
