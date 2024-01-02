d2b3194c1232ce9f1adc56d56f4fef3ec3bb1a0b
Spondon33 <nurulaminkh33@gmail.com>
2024-01-02 19:51:30 +0600

    errors

--- Libraries/libraries.py
+++ Libraries/libraries.py
@@ -55,3 +55,14 @@ try:
     print("hello, my name is", sys.argv[1])
 except IndexError:
     print("Too few arguments")
+
+
+# using conditionals to handle errors
+import sys
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
