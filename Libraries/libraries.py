5d0b38930d8de894d359159a6c33471c5c2e0cdd
Spondon33 <nurulaminkh33@gmail.com>
2024-01-03 10:41:39 +0600

    sys.exit

--- Libraries/libraries.py
+++ Libraries/libraries.py
@@ -66,3 +66,14 @@ elif len(sys.argv) > 2:
     print("Too many arguments")
 else:
     print("hello, my name is", sys.argv[1])
+
+
+# sys.exit = will exit the program.
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+
+print("hello, my name is", sys.argv[1])
