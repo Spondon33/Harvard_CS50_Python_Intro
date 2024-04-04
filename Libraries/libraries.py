249345d901dde072150a2252a2ed9c1d0cf21520
Spondon33 <nurulaminkh33@gmail.com>
2024-04-05 00:43:52 +0600

    custom libraries

--- Libraries/libraries.py
+++ Libraries/libraries.py
@@ -149,3 +149,11 @@ obj = response.json()
 for result in obj["results"]:
     print(result["trackName"])
 
+import sys
+
+from sayings import hello
+
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
+
+
