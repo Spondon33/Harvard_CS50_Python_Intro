f3dea4bfbd5c2aa0f431d735b014420fa579ad92
Spondon33 <nurulaminkh33@gmail.com>
2024-04-05 00:09:40 +0600

    retrieving specific data

--- Libraries/libraries.py
+++ Libraries/libraries.py
@@ -134,3 +134,18 @@ if len(sys.argv) != 2:
 response = requests.get("https://itunes.apple.com/%20search?entity=song&limit=1&term=" + sys.argv[1])
 print(json.dumps(response.json(), indent=2))
 
+
+# retrieving specific datas
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/%20search?entity=song&limit=1&term=" + sys.argv[1])
+
+obj = response.json()
+for result in obj["results"]:
+    print(result["trackName"])
+
