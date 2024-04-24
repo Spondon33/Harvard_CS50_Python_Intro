992a96fbb12ad85391a782d88fcd643ba5a24160
Nurul Amin Khondoker <94747094+Spondon33@users.noreply.github.com>
2024-04-24 23:22:49 +0600

    Add files via upload

---
+++ Problem Sets/Problem Set 1/deep.py
@@ -0,0 +1,9 @@
+ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+if ans == str(42):
+    print("Yes")
+elif ans == "forty-two":
+    print("Yes")
+elif ans == "forty two":
+    print("Yes")
+else:
+    print("No")
