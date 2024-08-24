efff894965e15ecf454ab4198d0d360021e502c9
Spondon33 <nurulaminkh33@gmail.com>
2024-08-24 23:11:12 +0600

    with

--- File IO/names.py
+++ File IO/names.py
@@ -50,3 +50,11 @@ name = input("What's your name? ")
 file = open("names.txt", "a")
 file.write(f"{name}\n")
 file.close()
+
+
+# 7
+# with
+name = input("What's your name? ")
+
+with open("names.txt", "a") as file :
+    file.write(f"{name}\n")
