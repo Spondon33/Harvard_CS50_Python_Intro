0f0873f3639d37f1a06542863acdf495b94ec9e2
Spondon33 <nurulaminkh33@gmail.com>
2024-08-24 17:57:00 +0600

    sort

--- File IO/names.py
+++ File IO/names.py
@@ -1,3 +1,23 @@
 # 1
 name = input("Whats your name? ")
 print(f"Hello, {name}")
+
+
+# 2
+names = []
+
+for _ in range(3):
+    name = input("Whats your name? ")
+    names.append(name)
+
+print(f"Hello, {names}")
+
+
+# 3
+names = []
+
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+for name in sorted(names):
+    print(f'Hello, {name}')
