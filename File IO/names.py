32bd2d6afaea1d4d7539652890708361453663f2
Spondon33 <nurulaminkh33@gmail.com>
2024-08-24 23:03:17 +0600

    open

--- File IO/names.py
+++ File IO/names.py
@@ -26,9 +26,27 @@ for name in sorted(names):
 # storing Information
 
 # open : opens a file in order to read or write
+# w = write. It will rewrite the whole file everytime
 # 4
 name = input("Whats your name? ")
 
 file = open("names.txt", 'w')
 file.write(name)
+file.close()
+
+
+# 5
+# a = append. It will append the data each time with the previous data.
+name = input("What's your name? ")
+
+file = open("names.txt", "a")
+file.write(name)
+file.close()
+
+
+# 6
+name = input("What's your name? ")
+
+file = open("names.txt", "a")
+file.write(f"{name}\n")
 file.close()
