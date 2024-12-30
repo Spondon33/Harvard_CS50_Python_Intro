164945a0b2b27ae4cdc86c532034037ebef6f322
Spondon33 <nurulaminkh33@gmail.com>
2024-12-30 21:58:42 +0600

    ...

--- Regular Expressions/validate.py
+++ Regular Expressions/validate.py
@@ -1,72 +1,149 @@
-# 1
-email = input("What's your email? ").strip()
-
-if "@" in email:
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# 2
-email = input("What's your email? ").strip()
-
-if "@" in email and "." in email:
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# 3
-email = input("What's your email? ").strip()
-
-username, domain = email.split("@")
-
-if username and "." in domain:
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# 4
-email = input("What's your email? ").strip()
-
-username, domain = email.split("@")
-
-if username and domain.endswith(".edu"):
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# re library:
-# 5
+# # 1
+# email = input("What's your email? ").strip()
+#
+# if "@" in email:
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 2
+# email = input("What's your email? ").strip()
+#
+# if "@" in email and "." in email:
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 3
+# email = input("What's your email? ").strip()
+#
+# username, domain = email.split("@")
+#
+# if username and "." in domain:
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 4
+# email = input("What's your email? ").strip()
+#
+# username, domain = email.split("@")
+#
+# if username and domain.endswith(".edu"):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # re library:
+# # 5
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search("@", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 6
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(".+@.+", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 7
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r".+@.+\.edu", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 8
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^.+@.+\.edu$", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 9
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^[^@]+@[^@]+\.edu$", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 10
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 11
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^\w+@\w+\.edu$", email):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 12
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^\w+@\w+\.edu$", email, re.IGNORECASE):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 13
+# import re
+#
+# email = input("What's your email? ").strip()
+#
+# if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
+#     print("Valid")
+# else:
+#     print("Invalid")
+#
+#
+# # 14
 import re
 
 email = input("What's your email? ").strip()
 
-if re.search("@", email):
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# 6
-import re
-
-email = input("What's your email? ").strip()
-
-if re.search(".+@.+", email):
-    print("Valid")
-else:
-    print("Invalid")
-
-
-# 7
-import re
-
-email = input("What's your email? ").strip()
-
-if re.search(r".+@.+\.edu", email):
+if re.search(r"^\w+@(\w+\.)?\w+\.edu$", email, re.IGNORECASE):
     print("Valid")
 else:
     print("Invalid")
